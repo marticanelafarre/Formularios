795a62494fab2d78e77ddf568f5911b5bc508505
Marti Canela <marticanela2015@gmail.com>
2020-10-23 20:51:40 +0200

    inici

---
+++ src/app/componente1/componente1.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente1',
+  templateUrl: './componente1.component.html',
+  styleUrls: ['./componente1.component.css']
+})
+export class Componente1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  erDNI = '^[0-9]{8}[A-Aa-z]$';
+  mostrarIntro ="";
+  mostrarIntro2 ="";
+
+
+  EnviarDatos(nombre, dni, texto){
+    if(nombre.value == ""){
+      alert("El nombre esta vacio");
+    }
+    if(dni.match(this.erDNI)){
+      this.mostrarIntro="Correcto";
+    } else{
+      this.mostrarIntro="Incorrecto";
+    }
+    if(texto.value>2){
+      this.mostrarIntro2="demasiadoCorto";
+    }
+  }
+}
